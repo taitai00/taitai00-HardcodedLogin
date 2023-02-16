360ae95cdec83658d7e594c7518e7e0fb01eb66e
Khup Taithul <aatuang@gmail.com>
2023-02-16 00:39:33 +0000

    fixed the login method to return the correct result

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,8 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if(username.equals("admin") && password.equals("qwerty")) return true;
+        if(username.equals("user") && password.equals("password")) return true;
         return false;
     }
 }
